50b2a36125fa227be8b41b31e613a789c6951602
Hasnain_Raza <hr779656@gmail.com>
2023-06-05 11:06:40 +0500

    change schema

--- Model/Schema.js
+++ Model/Schema.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const userLogin = new mongoose.Schema({
     username:{
         type:String,
-        required:true
+        required:true,
+        unique:true
     },
     password:{
         type:String,
