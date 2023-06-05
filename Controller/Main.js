4ba96921f49696e60c63b112978fbb758d19aa17
Hasnain_Raza <hr779656@gmail.com>
2023-06-05 11:00:26 +0500

    rechanges

--- Controller/Main.js
+++ Controller/Main.js
@@ -3,7 +3,7 @@ const Main_router = express.Router()
 // const auth_model = require("../Model/Model"
 
 
-Main_router.get("/api", async (req, res)=>{
+Main_router.get("/", async (req, res)=>{
     res.status(200).json({
         userLogin:"https://hr-textile-dashboard.cyclic.app/api/user-login",
         adminLogin:"https://hr-textile-dashboard.cyclic.app/api/admin-login"
