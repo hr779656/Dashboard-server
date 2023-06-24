b05039c5f9d1f1b6d7dedfd9ba11de1e9e60a64a
Hasnain_Raza <hr779656@gmail.com>
2023-06-24 13:22:05 +0500

    user_signup_changes

--- Model/Schema.js
+++ Model/Schema.js
@@ -7,8 +7,8 @@ const userSignup = new mongoose.Schema({
     },
     email:{
         type:String,
+        unique:true,
         required:true
-        
     },
     password:{
         type:String,
