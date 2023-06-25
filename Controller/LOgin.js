d62d17b4e20384e54f718d01f4d293cdf8b1a735
Hasnain_Raza <hr779656@gmail.com>
2023-06-25 15:53:13 +0500

    Secretkey_validation

--- Controller/LOgin.js
+++ Controller/LOgin.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const auth_model = require("../Model/Model")
+require("dotenv").config()
 
 
 router.post("/user-signup", async (req, res)=>{
@@ -22,23 +23,25 @@ router.post("/user-signup", async (req, res)=>{
 })
 
 
-router.post("/admin-signup", async (req, res)=>{
-
-        const adminSignup = await new auth_model.adminSignup_model({
-            username:req.body.username,
-            password:req.body.password,
-            email:req.body.email,
-            Secretkey:req.body.Secretkey
-        })
-
-        try{
-            const output = await adminSignup.save();
-            res.status(200).json(output)
-        }catch{
-            res.status(404).send("admin Not Saved In data bases")
-        }
-
-})
+router.post("/admin-signup", async (req, res) => {
+    if (req.body.Secretkey === process.env.Secretkey) {
+      try {
+        const adminSignup = new auth_model.adminSignup_model({
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+          Secretkey: req.body.Secretkey
+        });
+  
+        const output = await adminSignup.save();
+        res.status(200).json(output);
+      } catch {
+        res.status(404).send("admin not saved in data base");
+      }
+    } else {
+      res.status(404).send("your secret key is not correct");
+    }
+  });
 
 
 router.post("/user-login", async (req, res)=>{
